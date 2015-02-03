3a90c85512c779cec6492703a068dddc7133c643
zuaa <zuaa@163.com>
2015-02-03 21:43:16 +0800

    修改admin下user dao 测试log

--- model/admin/user/dao.js
+++ model/admin/user/dao.js
@@ -1,25 +1,24 @@
 var mysql = require('mysql');
 var crypto = require('crypto');
 var c = require('../../../config/config')
-var m=eval("c.config."+c.config.config+".mysql")
-console.log(m) 
-//填写数据库连接信息，可查询数据库详情页  
+	var m = eval("c.config." + c.config.config + ".mysql")
+	var log = eval("c.config." + c.config.config + ".log")
+	log.info(m);
+
 var moption = {
-		host: m.db_host,
-		port: m.db_port,
-		user: m.username,
-		password: m.password,
-		database: m.db_name
-	}
-	 
-exports.saveUrl = login;
- 
- 
-function login(obj, cb) {
- console.log(md5(obj))
+	host : m.db_host,
+	port : m.db_port,
+	user : m.username,
+	password : m.password,
+	database : m.db_name
 }
 
+exports.login = login;
 
-function md5 (text) {
-  return crypto.createHash('md5').update(text).digest('hex');
+function login(obj, cb) {
+	log.info(md5(obj))
+}
+
+function md5(text) {
+	return crypto.createHash('md5').update(text).digest('hex');
 };
