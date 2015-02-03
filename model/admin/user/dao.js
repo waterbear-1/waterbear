e3bc33541a9c297c89db9aad9dd7e72305c7fd85
zuaa <zuaa@163.com>
2015-02-03 22:08:51 +0800

    修改 dao

--- model/admin/user/dao.js
+++ model/admin/user/dao.js
@@ -5,7 +5,7 @@ var c = require('../../../config/config')
 	var log = eval("c.config." + c.config.config + ".log")
 	log.info(m);
 
-var moption = {
+var option = {
 	host : m.db_host,
 	port : m.db_port,
 	user : m.username,
@@ -14,11 +14,58 @@ var moption = {
 }
 
 exports.login = login;
+exports.reg = reg;
 
-function login(obj, cb) {
-	log.info(md5(obj))
+
+function reg(){
+	
 }
 
-function md5(text) {
-	return crypto.createHash('md5').update(text).digest('hex');
+
+/* 用户验证，用户信息 */
+function login(obj, cb) {
+	if(obj){
+		if(obj.psw){
+			obj.psw=md5(obj.psw+"")
+		}
+	}
+	openConn(function(client){
+		log.info(obj.psw);
+		/* 这里增加一个sql语句，用于检查用户密码是否匹配 */
+		
+		client.query(
+			'select * from wb_url_base where ? ,?', [obj.user,obj.psw],
+			function(err, results) {
+				if (err) {
+					console.log(err);
+					return;
+				}
+				if (results.length > 0) {
+					cb(true);
+					client.end();
+				} else {
+					cb(false);
+					client.end();
+				}
+			}
+		);
+		//关闭数据库连接
+		
+		cb(true);
+	})
+	
+}
+/* md5 加盐 */
+function md5(text) {   
+    var one=one+crypto.createHash('md5').update(text).digest('hex');
+	return crypto.createHash('md5').update(one).digest('hex');
 };
+
+
+/* 用来方便打开数据库 */
+function openConn(cb) {
+	var client = mysql.createConnection(option);
+	cb(client);
+}
+
+
