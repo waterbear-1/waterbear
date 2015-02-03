b67e0e65723716b07be80132b7e0af72571a4328
zuaa <zuaa@163.com>
2015-02-03 12:17:42 +0800

    a

---
+++ config/config.js
@@ -0,0 +1,20 @@
+exports.config = {
+	config:"develop", 
+	develop:{
+		mysql:{
+			host:"192.168.3.242",
+			port:3306,
+			user:"root",
+			psw:"123456"
+		}
+	},
+	product:{
+		mysql:{
+			host:"192.168.3.2421111",
+			port:33061,
+			user:"root1",
+			psw:"1234561"
+		}
+	}
+	
+}
