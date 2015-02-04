807d3d9dbb87d608bf97dff6da34a7bc74a897ea
zuaa <zuaa@163.com>
2015-02-04 08:46:16 +0800

    peizhiwe

--- config/config.js
+++ config/config.js
@@ -1,13 +1,15 @@
+var Log = require('log');
 exports.config = {
 	config:"develop", 
 	develop:{
 		mysql:{
 			username:"root",
-			password:"",
+			password:"123456",
 			db_host:"192.168.3.242",
 			db_port:3306,
 			db_name:"zuaa"
-		}
+		},
+		log:new Log('info')
 	}, 
 	product:{
 		mysql:{
@@ -16,7 +18,7 @@ exports.config = {
 			db_host:"svrid0r041bke62.mysql.duapp.com",
 			db_port:10242,
 			db_name:"svrid0r041bke62"
-		}
-	}
-	
+		},
+		log:new Log('error')
+	}  
 }
