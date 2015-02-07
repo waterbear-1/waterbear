6e5439b998e93f38f68b60339fe237cdac8e0544
zuaa <zuaa@163.com>
2015-02-07 14:50:40 +0800

    修改用户的service

--- model/admin/user/dao.js
+++ model/admin/user/dao.js
@@ -1,74 +1,65 @@
 var mysql = require('mysql');
 var crypto = require('crypto');
 var c = require('../../../config/config');
-	var m = eval("c.config." + c.config.config + ".mysql");
-	var log = eval("c.config." + c.config.config + ".log");
-	log.info(m);
+var m = eval("c.config." + c.config.config + ".mysql");
+var log = eval("c.config." + c.config.config + ".log");
+log.debug(m);
 
 var option = {
-	host : m.db_host,
-	port : m.db_port,
-	user : m.username,
-	password : m.password,
-	database : m.db_name
+	host: m.db_host,
+	port: m.db_port,
+	user: m.username,
+	password: m.password,
+	database: m.db_name
 }
 
 exports.login = login;
 exports.reg = reg;
 
 
-function reg(obj,cb){
-	openConn(function(client){
+function reg(obj, cb) {
+	openConn(function(client) {
 		//TODO 
-		log.info("reg:"+obj.user);
+		log.info("reg:" + obj.user);
 	})
 }
 
 
-/* 用户验证，用户信息 */
 function login(obj, cb) {
-	if(obj){
-		if(obj.psw){
-			obj.psw=md5(obj.psw+"")
+	if (obj) {
+		if (obj.psw) {
+			obj.psw = md5(obj.psw + "")
 		}
 	}
-	openConn(function(client){
-		log.info(obj.psw);
-		/* 这里增加一个sql语句，用于检查用户密码是否匹配 */
-		//TODO 
+	openConn(function(client) {
+		log.debug(obj);
 		client.query(
-			'select * from wb_url_base where ? ,?', [obj.user,obj.psw],
+			'select  *  from   user_base   where     login_name= ? and login_psw=?', [obj.name, obj.psw],
 			function(err, results) {
+				log.debug("i get the db message is ", results)
 				if (err) {
-					console.log(err);
+					log.error(err);
 					return;
 				}
 				if (results.length > 0) {
 					cb(true);
-					client.end();
 				} else {
 					cb(false);
-					client.end();
 				}
+				client.end();
 			}
 		);
-		//关闭数据库连接
-		
-		cb(true);
 	})
-	
+
 }
-/* md5 加盐 */
-function md5(text) {   
-    var one=one+crypto.createHash('md5').update(text).digest('hex');
+
+function md5(text) {
+	var one = one + crypto.createHash('md5').update(text).digest('hex');
 	return crypto.createHash('md5').update(one).digest('hex');
 };
 
 
-/* 用来方便打开数据库 */
 function openConn(cb) {
 	var client = mysql.createConnection(option);
 	cb(client);
 }
-
-
--- model/admin/user/testDao.js
+++ model/admin/user/testDao.js
@@ -2,7 +2,7 @@ var dao = require('./dao');
 var Log =require('log');
 var log =new Log('info');
 
-dao.login({"obj":111,"psw":"1112"},function(data){
-	log.info(data)
+dao.login({"name":"admin","psw":"admin"},function(data){
+	log.info("test  :"+data)
 })
 
